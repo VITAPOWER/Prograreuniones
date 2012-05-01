967824c4ba30222a525ae44327d7b13ca95c40bb
Chain <Chain@Chain-PC>
2012-05-01 01:01:58 -0500

    Agregar comentarios al java

--- src/java/action/Login.java
+++ src/java/action/Login.java
@@ -1,50 +1,80 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *Proyecto: Programa Reuniones para Ramon Brena
+ * 
+ *Alumnos/Autores que participaron:
+ *Adrián M Morales 615525                                                   
+ *Eder Ramírez 796598
+ *Daniel Sánchez 617763
+ *Carolina Flores 617680
+ * 
+ *Materia: Proyecto Integrador 9nvo semestre
+ *Profesor: Dr.Lorena Gomez y Dr.Juan Carlos Lavariega
+ *
+ * Nombre de archivo: Votacion.java
+ * Fecha Ultima Modificacion: Abril, 30, 2012
+ * 
+ * 
+ * Descripcion: clase para hacer hacer un login.
+ */
+
+/*Paquete:
+ * Nombrar paquete al que pertenece el archivo.
  */
 package action;
 
-import Daos.UsuarioDAO;
-import Pojos.Usuario;
+/*
+ * Librerias: Importar librerias utilizadas
+ */
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+//importar Daos
+import Daos.UsuarioDAO;
+//importar pojos
+import Pojos.Usuario;
 
-/**
- *
- * @author snidE
+/*
+ * Inicio de la clase: Descripcion: esta clase permite hacer login.
  */
-public class Login extends ActionSupport implements ModelDriven{
+public class Login extends ActionSupport implements ModelDriven {
 
+    //declarar clases globales
     private Usuario user = new Usuario();
+    //declarar variables globales
     private String message;
-    
+
     @Override
     public String execute() throws Exception {
         message = null;
-       
+
         return SUCCESS;
 
     }
     
+    //metodo que hace el login
     public String login() {
         UsuarioDAO userDAO = new UsuarioDAO();
         message = "Usuario o Password invalido!";
         List<Usuario> result = userDAO.findByExample(user);
-        if(result.isEmpty()) {
-            
+        /*Verificar usuario, si el usuario no existe regresa a la 
+         * pantalla inicial.
+         */
+        if (result.isEmpty()) {
+
             return SUCCESS;
         }
+        //Meter el usuario en sesion.
         Map session = ActionContext.getContext().getSession();
-            session.put("context", new Date());
-            session.put("nombreusuario", result.get(0).getNombre());
-            session.put("idusuario", result.get(0).getIdusuario());
+        session.put("context", new Date());
+        session.put("nombreusuario", result.get(0).getNombre());
+        session.put("idusuario", result.get(0).getIdusuario());
         return "successlogin";
     }
-    
+
+    //metodos get
     @Override
     public Object getModel() {
         return user;
@@ -54,16 +84,16 @@ public class Login extends ActionSupport implements ModelDriven{
         return user;
     }
 
-    public void setUser(Usuario user) {
-        this.user = user;
-    }
-
     public String getMessage() {
         return message;
+    }
+
+    //metodos set
+    public void setUser(Usuario user) {
+        this.user = user;
     }
 
     public void setMessage(String message) {
         this.message = message;
     }
-    
 }
