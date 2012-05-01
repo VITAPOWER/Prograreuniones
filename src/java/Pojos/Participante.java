980e30d0f403b19876d0790f0e251dc0e180fb77
Chain <Chain@Chain-PC>
2012-05-01 03:06:16 -0500

    Agregar comentarios al java

--- src/java/Pojos/Participante.java
+++ src/java/Pojos/Participante.java
@@ -1,15 +1,29 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *Proyecto: Programa Reuniones para Ramon Brena
+ * 
+ *Alumnos/Autores que participaron:
+ *Adrián M Morales 615525                                                   
+ *Eder Ramírez 796598
+ *Daniel Sánchez 617763
+ *Carolina Flores 617680
+ * 
+ *Materia: Proyecto Integrador 9nvo semestre
+ *Profesor: Dr.Lorena Gomez y Dr.Juan Carlos Lavariega
+ *
+ * Nombre de archivo: Participante.java
+ * Fecha Ultima Modificacion: Abril, 30, 2012
+ * 
+ * 
+ * Descripcion: clase participante.
+ */
+
+/*Paquete:
+ * Nombrar paquete al que pertenece el archivo.
  */
 package Pojos;
 
-/**
- *
- * @author Carolina
- */
 public class Participante {
-    
+    //declarar variables globales
     private Integer idparticipantes;
     private String email;
     private Integer idreunion;
@@ -17,53 +31,54 @@ public class Participante {
     private Integer evitar;
     private Integer apoyar;
     
+    //metodos get
     public Integer getIdparticipantes() {
         return idparticipantes;
-    }
-
-    public void setIdparticipantes(Integer idparticipantes) {
-        this.idparticipantes = idparticipantes;
     }
     
     public Integer getApoyar() {
         return apoyar;
     }
 
-    public void setApoyar(Integer apoyar) {
-        this.apoyar = apoyar;
-    }
-
     public Integer getBloquear() {
         return bloquear;
-    }
-
-    public void setBloquear(Integer bloquear) {
-        this.bloquear = bloquear;
     }
 
     public Integer getEvitar() {
         return evitar;
     }
 
-    public void setEvitar(Integer evitar) {
-        this.evitar = evitar;
-    }
-
     public Integer getIdreunion() {
         return idreunion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    //metodos set
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setIdreunion(Integer idreunion) {
         this.idreunion = idreunion;
     }
 
-    public String getEmail() {
-        return email;
+    public void setEvitar(Integer evitar) {
+        this.evitar = evitar;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setBloquear(Integer bloquear) {
+        this.bloquear = bloquear;
     }
-    
+
+    public void setApoyar(Integer apoyar) {
+        this.apoyar = apoyar;
+    }
+
+    public void setIdparticipantes(Integer idparticipantes) {
+        this.idparticipantes = idparticipantes;
+    }
     
 }
