10ba9aa81fba5eb9393a4f3bdcf7d48fff3307a3
Chain <Chain@Chain-PC>
2012-05-01 01:01:54 -0500

    Agregar comentarios al java

--- src/java/action/Logout.java
+++ src/java/action/Logout.java
@@ -1,16 +1,37 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *Proyecto: Programa Reuniones para Ramon Brena
+ * 
+ *Alumnos/Autores que participaron:
+ *Adrián M Morales 615525                                                   
+ *Eder Ramírez 796598
+ *Daniel Sánchez 617763
+ *Carolina Flores 617680
+ * 
+ *Materia: Proyecto Integrador 9nvo semestre
+ *Profesor: Dr.Lorena Gomez y Dr.Juan Carlos Lavariega
+ *
+ * Nombre de archivo: Votacion.java
+ * Fecha Ultima Modificacion: Abril, 30, 2012
+ * 
+ * 
+ * Descripcion: clase para hacer hacer un logout.
+ */
+
+/*Paquete:
+ * Nombrar paquete al que pertenece el archivo.
  */
 package action;
+/*
+ * Librerias: Importar librerias utilizadas
+ */
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import java.util.Map;
 
-/**
- *
- * @author Carolina
+/*
+ * Inicio de la clase: 
+ * Descripcion: esta clase permite hacer logout.
  */
 public class Logout extends ActionSupport{
     
