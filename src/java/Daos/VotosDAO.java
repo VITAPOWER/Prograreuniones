ed3b3ac44403033c57e78bc76f21d9479a6600fb
Chain <Chain@Chain-PC>
2012-05-01 03:17:17 -0500

    Agregar comentarios al java

--- src/java/Daos/VotosDAO.java
+++ src/java/Daos/VotosDAO.java
@@ -1,13 +1,29 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *Proyecto: Programa Reuniones para Ramon Brena
+ * 
+ *Alumnos/Autores que participaron:
+ *Adrián M Morales 615525                                                   
+ *Eder Ramírez 796598
+ *Daniel Sánchez 617763
+ *Carolina Flores 617680
+ * 
+ *Materia: Proyecto Integrador 9nvo semestre
+ *Profesor: Dr.Lorena Gomez y Dr.Juan Carlos Lavariega
+ *
+ * Nombre de archivo: VotosDAO.java
+ * Fecha Ultima Modificacion: Abril, 30, 2012
+ * 
+ * 
+ * Descripcion: dao para votos.
+ */
+
+/*Paquete:
+ * Nombrar paquete al que pertenece el archivo.
  */
 package Daos;
+//importar pojo
 import Pojos.Votos;
-/**
- *
- * @author Chain
- */
+
 public class VotosDAO extends GenericDAO<Votos, Integer>{
     
 }
